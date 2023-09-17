2feb92cd6eda6e54e6e2f7d9c32b854521a2ed87
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2023-09-17 12:52:27 +0800

    initial commit

---
+++ MachineProblem1-ITELEC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MachineProblem1_ITELEC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
